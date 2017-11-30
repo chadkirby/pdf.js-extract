59e5dea9103ecff713abaf03ba6a0c343aa65799
Chad Kirby <chad.kirby@gmail.com>
2017-11-30 11:17:21 -0800

    allow option to specify which pages to extract text from

--- example/example-buffer.js
+++ example/example-buffer.js
@@ -2,12 +2,12 @@ var fs = require('fs');
 var assert = require('assert');
 var PDFExtract = require('../lib').PDFExtract;
 var pdfExtract = new PDFExtract();
-var buffer = fs.readFileSync('./example.pdf');
+var buffer = fs.readFileSync(`${__dirname}/example.pdf`);
 pdfExtract.extractBuffer(buffer, {}, function(err, data) {
   if (err) {
     return console.log(err);
   }
-  var expected = require('./example-output.json');
+  var expected = require(`${__dirname}/example-output.json`);
   assert.deepEqual(data.meta, expected.meta);
   assert.deepEqual(data.pages, expected.pages);
 });
--- example/example.js
+++ example/example.js
@@ -1,8 +1,10 @@
 var fs = require('fs');
 var PDFExtract = require('../lib').PDFExtract;
 var pdfExtract = new PDFExtract();
-pdfExtract.extract('./example.pdf', {} /* options, currently nothing available*/, function (err, data) {
-	if (err) return console.log(err);
-	fs.writeFileSync('./example-output.json', JSON.stringify(data, null, '\t'));
-	console.log(JSON.stringify(data, null, '\t'));
+pdfExtract.extract(`${__dirname}/example.pdf`, { firstPage: 1, lastPage: 1 }, function(err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  fs.writeFileSync(`${__dirname}/example-output.json`, JSON.stringify(data, null, '\t'));
+  console.log(JSON.stringify(data, null, '\t'));
 });
